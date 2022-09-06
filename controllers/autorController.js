397b0586bd2646dc03dedc785addaa9df56ee247
gabi-resende <gabrieliresende00@gmail.com>
2022-09-06 11:37:02 -0300

    listarautor

--- controllers/autorController.js
+++ controllers/autorController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarAutor(request, response){
         try {
-            return response.status(200).json({confirma: 'Autor'});
+            const sql = 'select codaut_id, nomeaut, dat_nascimento, loc_nascimento, foto, biografia from autor;';
+            const autor = await db.query(sql);
+            return response.status(200).json({confirma: 'sucesso', nresults: autor[0]. length, message: autor[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
