3cb99c543be15191e0cb279bdc77b12e93429c2f
gabi-resende <gabrieliresende00@gmail.com>
2022-09-08 09:37:13 -0300

    listareditora

--- controllers/editoraController.js
+++ controllers/editoraController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarEditora(request, response){
         try{
-            return response.status(200).json({confirma: 'Editora'});
+            const sql = 'select codedit_id, cnpj, nome_edit, local_edit;';
+            const editora = await db.query(sql);
+            return response.status(200).json({confirma: 'sucesso', nresults: editora[0]. length, message: editora[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
--- controllers/emprestimoLivroController.js
+++ controllers/emprestimoLivroController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarEmprestimoLivro(request, response){
         try{
-            return response.status(200).json({confirma: 'Emprestimo_livro'});
+            const sql = 'select emprest_id, codlivro_id, user_id, dt_emprest, dtt_devolucao, devolvido from esmprestimo_livro;';
+            const emprestimo_livro = await db.query(sql);
+            return response.status(200).json({confirma: 'sucesso', nresults: emprestimo_livro [0]. length, message: emprestimo_livro [0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
