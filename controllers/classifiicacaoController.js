e1062ef5c4eb0b5e3ab5587b9de0a63786ef5a11
analuizapavaodagama <gamaanaluiza743@gmail.com>
2022-09-08 09:36:23 -0300

    listarclassificacao

--- controllers/classifiicacaoController.js
+++ controllers/classifiicacaoController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarClassificacao(request, response){
         try{
-            return response.status(200).json({confirma: 'Classificacao'});
+            const sql = 'select classic_id, codclassifacao, descricao_classic from classificacao;';
+            const classificacao = await db.query(sql)
+            return response.status(200).json({confirma: 'sucesso', nresults: classificacao [0]. lenght, message: classificacao [0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
