3ca229ff324a28c76d2f2a5fb7540cd4f6ca680f
analuizapavaodagama <111364213+analuizapavaodagama@users.noreply.github.com>
2022-09-08 09:27:27 -0300

    Listar reserva_livro

--- controllers/reservaLivroController.js
+++ controllers/reservaLivroController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarReserva(request, response){
         try{
-            return response.status(200).json({confirma: 'Reserva'});
+            const sql = 'select reserv_id, dt_lista, codlivro_id, user_id, ativo from reserva_livro'
+            const reserva_livro =  await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: reserva_livro[0].length, message: reserva_livro[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async  listarUsuarios(request, response){
         try{
-            return response.status(200).json({confirma: 'Usuarios'});
+            const sql = 'select user_id, nome_user, dt_nascimento, rm, email, senha, tipo from usuarios;';
+            const Usuarios =  await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: Usuarios[0].length, message: Usuarios[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
